0cdb24633d1dc03993acb09760860602dbe2a2a5
Ana Oliveira <anaf.oliveira95@gmail.com>
2018-12-09 21:22:08 +0000

    Add files via upload

--- DropMusic_Upload.py
+++ DropMusic_Upload.py
@@ -32,24 +32,119 @@ class uploads:
                     conn.close()
                     return shown
 
-    def  addUpload(user, idM, ficheiro='', nome='', tipo_ficheiro=''):
-        sql = """INSERT INTO upload
-                VALUES (%s,%s,%s,%s,%s)"""
-        
+    def showUploads(user):
+        sql = """select nome, musica_musica_id from uploads where user_username=%s"""
+        shown=False
+
         try:
+
             conn = psycopg2.connect(host="localhost",database="musicas", user="postgres", password="1234")
             # create a new cursor
             cur = conn.cursor()
             # execute the INSERT statement
-            cur.execute(sql, (ficheiro,nome,tipo_ficheiro, user, idM))
+            cur.execute(sql, (user,))
             # commit the changes to the database
-            conn.commit()
-            # close communication with the database
+            row = cur.fetchone()
+            while row is not None:
+                print( row ) ##------------------------------------------------------------------------
+                row = cur.fetchone()
+                shown=True
+# close communication with the database
             cur.close()
+
+
+
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)##-------------------------------------------------------------------------------------------
             print ('Algo correu mal :( /n tentaremos resolver o problema no futuro')
         finally:
             if conn is not None:
                 conn.close()
-                
+                return shown
+##################################################################################################################3
+    def getListUploads(user):
+        sql = """select musica_musica_id from upload
+             where user_username=%s"""
+        shown=False
+
+        try:
+
+            conn = psycopg2.connect(host="localhost",database="musicas", user="postgres", password="1234")
+            # create a new cursor
+            cur = conn.cursor()
+            # execute the INSERT statement
+            cur.execute(sql, (user,))
+            # commit the changes to the database
+            row = cur.fetchone()
+            shown=True
+            while row is not None:
+                print( row ) ##------------------------------------------------------------------------
+                row = cur.fetchone()
+                shown=True
+# close communication with the database
+            cur.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)##-------------------------------------------------------------------------------------------
+            print ('Algo correu mal :( /n tentaremos resolver o problema no futuro')
+        finally:
+            if conn is not None:
+                conn.close()
+                return shown
+
+   
+    def addUpload(user, nome, idM, userIdShare, ficheiro='',tipo_ficheiro):
+        sqlCreate = """INSERT INTO upload
+             VALUES(%s,%s,%s,%s,%s)"""
+        sqlAdd= ("""INSERT INTO user_upload
+             VALUES(%s,%s,%s)""")
+
+
+        try:
+
+            conn = psycopg2.connect(host="localhost",database="musicas", user="postgres", password="1234")
+            # create a new cursor
+            cur = conn.cursor()
+            # execute the INSERT statement
+            cur.execute(sqlCreate, (ficheiro,nome,tipo_ficheiro,user,idM))
+            # commit the changes to the database
+            conn.commit()
+            # close communication with the database
+            cur.execute(sqlAdd, (userIdShare,user, idM))
+            conn.commit()
+
+            print('as alteracoes foram guardadas com sucesso!')
+            cur.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)##-------------------------------------------------------------------------------------------
+            print ('Algo correu mal :( /n tentaremos resolver o problema no futuro')
+        finally:
+            if conn is not None:
+                conn.close()
+    
+    def updateList(user, idM):
+        sqlPosicao= """update upload set musica_musica_id = %s where user_username = %s """
+        sql= """INSERT INTO posicaoplaylist
+             VALUES(%s,%s,%s,%s)"""
+
+
+        try:
+
+            conn = psycopg2.connect(host="localhost",database="musicas", user="postgres", password="1234")
+            cur = conn.cursor()
+            cur.execute(sqlPosicao, (idM,user))
+            row = cur.fetchone()   
+            cur.execute(sql, (row[0],nome,user, idM))
+            conn.commit()
+
+            print('as alteracoes foram guardadas com sucesso!')
+            cur.close()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)##-------------------------------------------------------------------------------------------
+            print ('Algo correu mal :( /n tentaremos resolver o problema no futuro')
+        finally:
+            if conn is not None:
+                conn.close()
+  
